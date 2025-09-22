efaa50a8f78636aa7a7f05143d9e236d85e2b6c7
Daniel Wagner <you@example.com>
2025-09-22 10:53:00 +0200

    added wpf delete jobApplication

--- AppTrack.Wpf/ViewModel/MainViewModel.cs
+++ AppTrack.Wpf/ViewModel/MainViewModel.cs
@@ -4,6 +4,7 @@ using AppTrack.WpfUi.WindowService;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace AppTrack.WpfUi.ViewModel
 {
@@ -33,11 +34,46 @@ namespace AppTrack.WpfUi.ViewModel
             var createJobApplicationViewModel = new CreateJobApplicationViewModel();
             var windowResult = _windowService.ShowWindow(createJobApplicationViewModel);
 
-            if(windowResult == true)
+            if(windowResult == false)
             {
-                var apiResponse = await _jobApplicationService.CreateJobApplication(createJobApplicationViewModel.JobApplication);
-                JobApplications.Add(apiResponse.Data);
+                return;
             }
+
+            var apiResponse = await _jobApplicationService.CreateJobApplication(createJobApplicationViewModel.JobApplication);
+
+            if(apiResponse.Success == false)
+            {
+                MessageBox.Show(apiResponse.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error); //MessageBoxService
+            }
+
+            JobApplications.Add(apiResponse.Data);
+            
+        }
+
+        [RelayCommand]
+        private async Task DeleteJobApplication(int id)
+        {
+            var dialogResult = MessageBox.Show($"Do you really want to delete application with id: {id}", "Delete application", MessageBoxButton.OKCancel, MessageBoxImage.Question); //MessageBoxService
+
+            if (dialogResult == MessageBoxResult.No || dialogResult == MessageBoxResult.Cancel)
+            {
+                return;
+            }
+
+            var apiResponse = await _jobApplicationService.DeleteJobApplication(id);
+
+            if (apiResponse.Success == false)
+            {
+                MessageBox.Show(apiResponse.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error); //MessageBoxService
+            }
+
+            var jobApplicationToRemove = JobApplications.SingleOrDefault(x => x.Id == id);
+
+            if (jobApplicationToRemove != null)
+            {
+                JobApplications.Remove(jobApplicationToRemove);
+            }
+
         }
     }
 }
--- AppTrack.Wpf/WindowService/WindowService.cs
+++ AppTrack.Wpf/WindowService/WindowService.cs
@@ -8,11 +8,21 @@ public class WindowService : IWindowService
 {
     public bool? ShowWindow<TViewModel>(TViewModel viewModel) where TViewModel : class
     {
+        var owner = Application.Current.Windows
+        .OfType<Window>()
+        .FirstOrDefault(w => w.IsActive);
+
         Window window = viewModel switch
         {
             CreateJobApplicationViewModel vm => new CreateJobApplicationView(vm),
             _ => throw new NotImplementedException()
         };
+
+        if (owner is not null)
+        {
+            window.Owner = owner;
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+        }
 
         return window.ShowDialog();
     }
