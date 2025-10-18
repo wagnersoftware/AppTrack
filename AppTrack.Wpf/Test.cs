878d0a34ff2c9ed6b781f05b79dd79cf64080816
Daniel Wagner <you@example.com>
2025-10-18 22:08:39 +0200

    Test push

---
+++ AppTrack.Wpf/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTrack.WpfUi
+{
+    internal class Test
+    {
+        public Test() { }
+    }
+}
