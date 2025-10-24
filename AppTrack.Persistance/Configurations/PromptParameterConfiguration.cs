a7158da72bee9431386654a3820dc619c6add14c
Daniel Wagner <you@example.com>
2025-10-24 16:57:25 +0200

    modified PromptParameter db configuration

---
+++ AppTrack.Persistance/Configurations/PromptParameterConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AppTrack.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AppTrack.Persistance.Configurations;
+
+public class PromptParameterConfiguration : IEntityTypeConfiguration<PromptParameter>
+{
+    public void Configure(EntityTypeBuilder<PromptParameter> builder)
+    {
+        builder.Property(x => x.Key)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.Value)
+            .IsRequired();
+
+        builder.HasIndex(x => new { x.AISettingsId, x.Key }) // key must be unique per AISettings
+            .IsUnique();
+    }
+}
