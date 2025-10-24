277008881eb064359f7d3b7790bc51773bb6873b
Daniel Wagner <you@example.com>
2025-10-24 16:23:33 +0200

    added AiSettings db configuraion

---
+++ AppTrack.Persistance/Configurations/AiSettingsConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using AppTrack.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AppTrack.Persistance.Configurations;
+
+public class AiSettingsConfiguration : IEntityTypeConfiguration<AiSettings>
+{
+    public void Configure(EntityTypeBuilder<AiSettings> builder)
+    {
+        builder.Property(x => x.ApiKey)
+        .HasMaxLength(200);
+    }
+}
