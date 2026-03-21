89938299bd06952ccfd530eef4730f280f2a3054
Daniel Wagner <daniel@wagnersoftware.net>
2026-03-22 00:39:12 +0100

    feat: add PromptModel

---
+++ Models/PromptModel.cs
@@ -0,0 +1,32 @@
+using AppTrack.Frontend.Models.Base;
+using AppTrack.Shared.Validation.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AppTrack.Frontend.Models;
+
+public partial class PromptModel : ModelBase, IPromptValidatable
+{
+    [ObservableProperty]
+    private string name = string.Empty;
+
+    [ObservableProperty]
+    private string promptTemplate = string.Empty;
+
+    public IEnumerable<PromptModel>? SiblingPrompts { get; set; }
+
+    public Guid TempId { get; set; } = Guid.NewGuid();
+
+    public PromptModel Clone()
+    {
+        return new PromptModel
+        {
+            Id = Id,
+            Name = Name,
+            PromptTemplate = PromptTemplate,
+            CreationDate = CreationDate,
+            ModifiedDate = ModifiedDate,
+            SiblingPrompts = SiblingPrompts,
+            TempId = TempId
+        };
+    }
+}
