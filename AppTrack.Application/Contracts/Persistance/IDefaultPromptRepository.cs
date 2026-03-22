2fd36f61d9cd86513b3e354b034b25d361b55420
Daniel Wagner <daniel@wagnersoftware.net>
2026-03-22 23:16:25 +0100

    docs: document GetByLanguageAsync as reserved for future multi-language support

--- AppTrack.Application/Contracts/Persistance/IDefaultPromptRepository.cs
+++ AppTrack.Application/Contracts/Persistance/IDefaultPromptRepository.cs
@@ -4,5 +4,8 @@ namespace AppTrack.Application.Contracts.Persistance;
 
 public interface IDefaultPromptRepository : IGenericRepository<DefaultPrompt>
 {
+    /// <summary>
+    /// Reserved for future use: filters default prompts by language once user language preference is implemented.
+    /// </summary>
     Task<IReadOnlyList<DefaultPrompt>> GetByLanguageAsync(string language);
 }
